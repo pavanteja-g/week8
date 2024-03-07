3ab1c7d3b32bc5eec16e49d9ce903e8740cb089b
Pavanteja-g <gpavanteja.gpt@gmail.com>
2024-03-07 10:08:57 +0530

    Changing this to delegate

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -7,17 +7,16 @@ public class MyStack<E> extends ArrayList<E> {
     private ArrayList<E> delegate;
     public MyStack()
     {
-        delegate = new ArrayList<>();
         delegate = this;
     }
     public void push(E e) {
-        add(e);
+        delegate.add(e);
     }
 
     public E pop() {
-        if (isEmpty()) throw new EmptyStackException();
-        E e = get(size() - 1);
-        remove(size() -1 );
+        if (delegate.isEmpty()) throw new EmptyStackException();
+        E e = delegate.get(delegate.size() - 1);
+        delegate.remove(delegate.size() -1 );
         return e;
     }
 
